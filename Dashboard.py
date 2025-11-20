ffccb88042b412b357e300cad32a639cccfd2a70
HOIARRTool <wilasinee.k@gmail.com>
2025-11-20 14:26:24 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -426,7 +426,7 @@ if df_filtered.empty:
 # 1. กำหนด Link รูปภาพ
 logo_urls = [
     "https://github.com/HOIARRTool/appqtbi/blob/main/messageImage_1763018963411.jpg?raw=true",    
-    "https://mfu.ac.th/fileadmin/_processed_/6/7/csm_logo_mfu_3d_colour_15e5a7a50f.png"
+    "https://github.com/HOIARRTool/appqtbi/blob/main/csm_logo_mfu_3d_colour_15e5a7a50f.png"
 ]
 
 # ใช้ HTML จัดวางแทน columns เพื่อความแม่นยำ
@@ -590,6 +590,7 @@ if target_col in df_filtered.columns:
         st.dataframe(suggestions_df, use_container_width=True, hide_index=True)
     else:
         st.info("ไม่พบข้อมูลความคาดหวังในช่วงข้อมูลที่เลือก")
+
 
 
 
